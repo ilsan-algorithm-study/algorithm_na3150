1da5b72f020e6fa65bc197e5ea96bd9e35e8aa1f
nayoung <64996121+na3150@users.noreply.github.com>
2022-04-23 13:42:34 +0900

    level33

--- level33/문제4-모범답안.cpp
+++ level33/문제4-모범답안.cpp
@@ -4,39 +4,39 @@ using namespace std;
 char vect[100];
 
 char uFind(char ch) {
-	if (vect[ch] == 0) return ch;
-	char ret = uFind(vect[ch]);
-	vect[ch] = ret;
-	return ret;
+ if (vect[ch] == 0) return ch;
+ char ret = uFind(vect[ch]);
+ vect[ch] = ret;
+ return ret;
 }
 
 void uUnion(char t1, char t2) {
-	char a = uFind(t1);
-	char b = uFind(t2);
-	if (a == b) return;
+ char a = uFind(t1);
+ char b = uFind(t2);
+ if (a == b) return;
 
-	if (a >= 'A' && a <= 'Z') { //A가 문자
-		vect[b] = a;
-	}
-	else {
-		vect[a] = b;
-	}
+ if (a >= 'A' && a <= 'Z') {
+  vect[b] = a;
+ }
+ else {
+  vect[a] = b;
+ }
 }
 
 int main()
 {
-	int n, k;
-	cin >> n >> k;
+ int n, k;
+ cin >> n >> k;
 
-	char a, b;
-	for (int i = 0; i < n; i++) {
-		cin >> a >> b;
-		uUnion(a, b);
-	}
+ char a, b;
+ for (int i = 0; i < n; i++) {
+  cin >> a >> b;
+  uUnion(a, b);
+ }
 
-	for (int i = 1; i <= k; i++) {
-		cout << uFind('0' + i);
-	}
+ for (int i = 1; i <= k; i++) {
+  cout << uFind('0' + i);
+ }
 
-	return 0;
+ return 0;
 }
