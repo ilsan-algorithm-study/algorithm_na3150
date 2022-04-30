b1eccea2d89bd2f9252f0560ca3ee0c18af78d9a
nayoung <64996121+na3150@users.noreply.github.com>
2022-04-30 13:17:44 +0900

    update

--- level35/문제10.cpp
+++ level35/문제10.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
+#include <vector>
 using namespace std;
+int cnt;
+
+void run(int x, int y)
+{
+	if (y == 7) {
+		cnt++;
+		return;
+	}
+	for (int i = -1; i <= 1; i++) //3가지 방향
+	{
+		int dx = x + i;
+		if (dx < 0 || dx>2) continue; //배열 범위 벗어나는 경우
+		run(dx, y + 1);
+	}
+}
 
 
 int main()
 {
-	string arr[6] = { "ABCD","ABCE","AGEH","EIEI","FEQE","ABAD" };
-	string str;
-	cin >> str;
-	int cnt = 0;
-	for (int i=0;i<n)
-
+	char a;
+	cin >> a;
+	run(a - 'A', 0);
+	cout << cnt;
 	return 0;
 }
